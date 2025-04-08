7a7d0ab966ff27ea7db779530905c9f589de85b4
Rafael Rios <rvrios22@gmail.com>
2025-04-08 06:39:22 -0700

    add: Footer with styles

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,28 +5,55 @@ function ContactForm() {
     e.preventDefault();
   };
   return (
-    <section className="contact-form-container">
+    <section id="contact" className="contact-form-container">
       <h2 className="sub-header">Ready To Talk?</h2>
       <form onSubmit={handleEmail} className="contact-form">
         <label htmlFor="name" className="contact-form-label general-text">
           Name:
         </label>
-        <input required type="text" id="name" className="contact-form-input general-text" />
+        <input
+          required
+          type="text"
+          id="name"
+          className="contact-form-input general-text"
+        />
         <label htmlFor="phone" className="contact-form-label general-text">
           Phone Number:
         </label>
-        <input required type="text" id="phone" className="contact-form-input general-text" />
+        <input
+          required
+          type="text"
+          id="phone"
+          className="contact-form-input general-text"
+        />
         <label htmlFor="email" className="contact-form-label general-text">
           Email:
         </label>
-        <input required type="text" id="email" className="contact-form-input general-text" />
+        <input
+          required
+          type="text"
+          id="email"
+          className="contact-form-input general-text"
+        />
         <label htmlFor="message" className="contact-form-label general-text">
           How Can We Help You?
         </label>
-        <textarea required name="message" id="message" className="contact-form-input general-text"></textarea>
-        <input type="submit" value="Submit" className="contact-form-input general-text" />
+        <textarea
+          required
+          name="message"
+          id="message"
+          className="contact-form-input general-text"
+        ></textarea>
+        <input
+          type="submit"
+          value="Submit"
+          className="contact-form-input general-text"
+        />
       </form>
-      <p className="general-text">Fill out the form above and we will reach out to you as soon as possible to schedule a FREE consultation.</p>
+      <p className="general-text">
+        Fill out the form above and we will reach out to you as soon as possible
+        to schedule a FREE consultation.
+      </p>
     </section>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import "../css/footer.css";
 
 function footer() {
   return (
-    <div>footer</div>
-  )
+    <footer>
+      <div className="footer-child">
+        <h6 className="sub-header">Contact us</h6>
+        <p className="general-text">
+          Phone: <a href="tel:+1-760-343-8622">1-760-343-8622</a>
+        </p>
+        <p className="general-text">
+          Email: <a href="mailto:">test@gmail.com</a>
+        </p>
+        <p className="general-text">
+          Address:{" "}
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            123 street
+          </a>
+        </p>
+      </div>
+      <div className="footer-child">
+        <h6 className="sub-header">Hours</h6>
+        <p className="general-text">Mondays - Fridays: 7AM - 3:30PM</p>
+        <p className="general-text">Saturdays: 10AM - 2PM</p>
+        <p className="general-text">Sundays: Closed</p>
+      </div>
+      <div className="footer-child">
+        <h6 className="sub-header">Social Media</h6>
+        <div className="footer-flex">
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="/instagram.svg" alt="instagram" height={35} />
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="/facebook.svg" alt="facebook" height={30} />
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="/google.svg" alt="google" height={30} />
+          </a>
+        </div>
+      </div>
+      <p className="general-text">
+        Designed and developed by{" "}
+        <a href="http://riosdev.com" target="_blank" rel="noopener noreferrer">
+          Riosdev.com
+        </a>
+      </p>
+    </footer>
+  );
 }
 
-export default footer
+export default footer;
